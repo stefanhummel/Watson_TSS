072a6a1b7200873e64b77b46910799affc8b086d
stefanhummel <stefan.hummel@gmail.com>
2023-01-10 17:12:11 +0100

    Create 00_TSS_parameters.sh
    1st version

---
+++ 00_TSS_parameters.sh
@@ -0,0 +1,13 @@
+### Watson Text to Speech
+###
+### set environment variables with TSS parameters
+
+# Watson Text to Speech API key
+TSS_APIKEY=""
+TSS_URL=""
+TSS_CUSTOM_ID_US=""
+TSS_CUSTOM_ID_GB=""
+TSS_CUSTOM_ID_DE=""
+TSS_CUSTOM_MODEL_US="custom_model_US.json"
+TSS_CUSTOM_MODEL_GB="custom_model_GB.json"
+
